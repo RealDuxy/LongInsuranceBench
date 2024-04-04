03fe49d4c1906aabcd07de7a04a414ba7c539c7b
RealDuxy <du.xi.yang.cool@gmail.com>
2024-04-04 21:30:26 +0800

    1. add bug test

--- run_preds_qwen.sh
+++ run_preds_qwen.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
-# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
-python pred_vllm.py --modelqwen15_4b_chat > qwen15_4b_chat.out 2>&1
-
-# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
-python pred_vllm.py --modelqwen15_7b_chat > qwen15_7b_chat.out 2>&1
-
-# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
-python pred_vllm.py --modelqwen15_14b_chat > qwen15_14b_chat.out 2>&1
-
+sleep 1
 # 运行main.py并传递第二个模型参数，将所有输出重定向到b.out
 python pred_vllm.py --model qwen15_14b_chat_int4 --quantize > qwen15_14b_chat_int4.out 2>&1
+
+# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
+python pred_vllm.py --model qwen15_4b_chat > qwen15_4b_chat.out 2>&1
+
+# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
+python pred_vllm.py --model qwen15_7b_chat > qwen15_7b_chat.out 2>&1
+
+# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
+python pred_vllm.py --model qwen15_14b_chat > qwen15_14b_chat.out 2>&1
+
+
 
