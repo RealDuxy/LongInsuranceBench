930b2adb34f1ce1d684a4cd27c99b4520c10babe
RealDuxy <paliss001@outlook.com>
2024-04-11 10:58:31 +0800

    添加vllm qwen系列的推理评估

--- run_preds_qwen.sh
+++ run_preds_qwen.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-sleep 1
-# 运行main.py并传递第二个模型参数，将所有输出重定向到b.out
-python pred_vllm.py --model qwen15_14b_chat_int4 --quantize > qwen15_14b_chat_int4.out 2>&1
 
 # 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
+python pred_vllm.py --model qwen15_0-5b_chat > qwen15_0-5b_chat.out 2>&1
+
+python pred_vllm.py --model qwen15_1-8b_chat > qwen15_1-8b_chat.out 2>&1
+
 python pred_vllm.py --model qwen15_4b_chat > qwen15_4b_chat.out 2>&1
 
-# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
 python pred_vllm.py --model qwen15_7b_chat > qwen15_7b_chat.out 2>&1
-
-# 运行main.py并传递第一个模型参数，将所有输出重定向到a.out
-python pred_vllm.py --model qwen15_14b_chat > qwen15_14b_chat.out 2>&1
 
 
 
